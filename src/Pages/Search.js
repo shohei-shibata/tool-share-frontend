17e2489a6d8220a36faac6e7ef52d3e401308889
Shohei Shibata <shohei.shibata.dev@gmail.com>
2019-10-04 06:06:37 -0400

    fetch groups list

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchGroup from '../Groups/SearchGroup.js'
 
+import requestApi from '../Utility/requestApi.js'
+
 function Search() {
+	const [ myGroups, setMyGroups ] = useState(requestApi.getMyGroups());
+	const myGroupsList = myGroups.map(group => {
+		return <li key={group._id.toString()}>{group.name}</li>
+	});
+
 	return (
 		<div>
 			<h2>Search</h2>
 			<SearchGroup />
 			<p>NOTE: We might want to do away with a Submit button and instead search as you type "onChange"</p>
+			<h3>My Groups:</h3>
+			<ul>{myGroupsList}</ul>
+			<h3>Searchable Tools:</h3>
+			<ul></ul>
 		</div>
 	);
 }
---
+++ src/Utility/requestApi.js
@@ -0,0 +1,11 @@
+import fakeGroups from '../mock_data/Groups.js'
+
+const requestApi = {
+
+	getMyGroups: (myUserId) => {
+		return fakeGroups
+	}
+
+}
+
+export default requestApi;
