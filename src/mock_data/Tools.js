b80ca06af62de63bc3ecbed51a6f2b5c5b751860
Shohei Shibata <shohei.shibata.dev@gmail.com>
2019-10-07 06:09:43 -0400

    working on search api interaction

---
+++ src/mock_data/Tools.js
@@ -0,0 +1,23 @@
+const fakeTools = [{
+    "_id": 1,
+    "name": "hammer",
+    "owner": 123,
+    "accessibleGroups": [9, 7],
+    "note": "hit the nail on the head"
+}, {
+    "_id": 2,
+    "name": "aerator",
+    "owner": 123,
+    "accessibleGroups": [9, 7],
+    "note": "put holes in ground"
+}, {
+    "_id": 3,
+    "name": "powerwash",
+    "owner": 123,
+    "accessibleGroups": [9, 7],
+    "note": "water sold separated"
+}];
+
+export default fakeTools;
+
+
